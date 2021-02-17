d95d382cf1770a39e5c231fb2de8ff733cc1352f
nelioyann <nanayannick1998@gmail.com>
2021-02-17 23:50:09 +0100

    first commit

---
+++ scripts/transitions.js
@@ -0,0 +1,78 @@
+// barba.init({
+//     transitions: [{
+//       name: 'opacity-transition',
+//       leave(data) {
+//         return gsap.to(data.current.container, {
+//           opacity: 0,
+//         });
+//       },
+//       enter(data) {
+//         return gsap.from(data.next.container, {
+//           opacity: 0
+//         });
+//       }
+//     }]
+//   });
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to(".loading-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut",
+  });
+
+  tl.to(".loading-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3,
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".animate-fade-up", {
+    duration: 1,
+    y: 30,
+    opacity: 0,
+    stagger: 0.4,
+    delay: 0.2,
+  });
+}
+
+  barba.init({
+    sync: true,
+
+    transitions: [
+      {
+        async leave(data) {
+          const done = this.async();
+
+          pageTransition();
+          await delay(1000);
+          done();
+        },
+
+        async enter(data) {
+          contentAnimation();
+        },
+
+        async once(data) {
+          contentAnimation();
+        },
+      },
+    ],
+  });
