0130f85b3f95fa32f13b189e52851964e3408061
nelioyann <nanayannick1998@gmail.com>
2021-03-15 13:18:34 +0100

    animations added

--- scripts/transitions.js
+++ scripts/transitions.js
@@ -26,7 +26,6 @@ function delay(n) {
 function pageTransition() {
   var tl = gsap.timeline();
 
-  
   tl.to(".loading-screen", {
     duration: 1.2,
     width: "100%",
@@ -39,7 +38,7 @@ function pageTransition() {
     width: "100%",
     left: "100%",
     ease: "Expo.easeInOut",
-    delay: 0.3,
+    delay: 0.4,
   });
   tl.set(".loading-screen", { left: "-100%" });
 }
@@ -52,35 +51,71 @@ function contentAnimation() {
     opacity: 0,
     stagger: 0.4,
     delay: 0.2,
-  });
-  tl.from("p, h3, figure", {
-    duration: 0.6,
-    opacity: 0,
-    stagger: 0.3,
-    delay: -1,
-  });
+  })
+  // .from("p, h3, figure", {
+  //   duration: 0.6,
+  //   opacity: 0,
+  //   stagger: 0.3,
+  // }, "-=5");
 }
 
-  barba.init({
-    sync: true,
+barba.init({
+  sync: true,
 
-    transitions: [
-      {
-        async leave(data) {
-          const done = this.async();
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
 
-          pageTransition();
-          await delay(1000);
-          done();
-        },
-
-        async enter(data) {
-          contentAnimation();
-        },
-
-        async once(data) {
-          contentAnimation();
-        },
+        pageTransition();
+        await delay(1000);
+        done();
       },
-    ],
-  });
+
+      async enter(data) {
+        contentAnimation();
+      },
+
+
+      // async once(data) {
+      //   contentAnimation();
+      // },
+
+
+    },
+  ],
+});
+
+
+
+const options = {
+  threshold: 0.3,
+  rootMargin: "0px",
+};
+
+const video = document.querySelector(".coffee-video")
+
+// JS: Play videos while they are visible in the viewport
+const videoObserver = new IntersectionObserver((entries, observer) => {
+  for (const entry of entries) entry.isIntersecting ? video.play() : video.pause();
+});
+
+for (const element of document.querySelectorAll('.js-viewport-aware-video')) {
+  videoObserver.observe(element);
+}
+
+
+const animationObserver = new IntersectionObserver((entries, observer)=>
+{
+  for (const entry of entries)
+  {
+    console.log(entry.target)
+    entry.target.classList.toggle("animated", entry.isIntersecting)
+  }
+}, options)
+
+// Use that IntersectionObserver to observe the visibility
+// of some elements
+for (const element of document.querySelectorAll('.build-in-animate')) {
+  animationObserver.observe(element);
+}
